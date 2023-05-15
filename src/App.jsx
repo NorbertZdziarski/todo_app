c660a96457b087354423a6829945703de2c2cdf4
NorbertZdziarski <norbertzdz@gmail.com>
2023-05-15 15:26:41 +0200

    show tasks data in JSX

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,82 @@ import {useEffect, useState} from "react";
 import {getAllTasks} from "./assets/helpers/api.js";
 
 function App() {
-  const [task, setTask] = useState([]);
+  const [tasks, setTasks] = useState([]);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
 
   useEffect(()=> {
     getAllTasks().then((data)=>{
-      setTask(data)
+      setTasks(data)
     })
         .catch(console.error)
   },[])
 
 
+  async function sendTaskData(data) {
+    const response = await fetch(
+        'http://localhost:3000/tasks',
+        {
+          headers: {
+            'Content-Type': 'application/json'},
+          method: 'POST',
+          body: JSON.stringify(data)
+          }
+    )
+    return response.json()
+  }
 
-
+async function handleSubmit(event){
+    event.preventDefault()
+    const result = await sendTaskData({
+      title, description, status: "open", addedDate: new Date()
+    })
+    setTitle('');
+    setDescription('')
+    setTasks([...tasks, result])
+}
 
 
   return (
     <>
+
+      <form
+        onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Title</label>
+          <input
+              value={title}
+              type="text"
+              id="title"
+              name="title"
+              onChange={(event) => setTitle(event.target.value)}
+              />
+        </div>
+        <div>
+          <label htmlFor="desc">Description</label>
+          <textarea
+              value={description}
+              id="desc"
+              name="desc"
+              onChange={(event) => setDescription(event.target.value)}
+              />
+        </div>
+        <button type="submit">Add</button>
+      </form>
+        <br/>
+      <section>
+          {tasks.map((task) => (
+              <div key={task.id}>
+                  <b>{task.title}</b> - <span>{task.description}</span>
+                  <button>Add operation</button>
+                  <button>Finish</button>
+                  <button>Delete</button>
+              </div>
+          ) )}
+
+      </section>
+
+
     </>
   )
 }
